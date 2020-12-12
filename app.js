41a66d7c00dcd9d907dfece72bf259878303257a
Nikhil Naik <naiknikhil925@gmail.com>
2020-12-12 18:17:46 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var textInput = document.getElementById("input-area");                                         
+var button = document.querySelector("#btn-translate")
+var outputDiv = document.querySelector("#output-area");
+
+var serverURL =  "https://api.funtranslations.com/translate/german-accent.json"
+
+function getRequiredURL(input){
+return serverURL + "?" + "text=" + input;
+}
+function errorHandler(error) {
+    alert("something wrong with server! try again after some time")
+}
+function clickhandler() {
+    var inputValue = textInput.value;
+    fetch(getRequiredURL(inputValue))
+    .then((response) => response.json())
+    .then((json) => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+button.addEventListener('click' , clickhandler);
+ 
+
